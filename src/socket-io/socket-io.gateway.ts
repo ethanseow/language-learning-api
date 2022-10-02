6ff391e3292f9ef358120b6cf398506ea457b9a2
Ethan Seow <eseow6088@bths.edu>
2022-10-01 23:14:48 -0400

    added vacant and empty room

--- src/socket-io/socket-io.gateway.ts
+++ src/socket-io/socket-io.gateway.ts
@@ -2,22 +2,52 @@ import {
     SubscribeMessage, 
     WebSocketGateway,
     WebSocketServer,
-    OnGatewayConnection
+    OnGatewayConnection,
+    ConnectedSocket,
+    MessageBody
 } from '@nestjs/websockets';
+import { Response } from 'express'
+import {Res} from '@nestjs/common'
 import { Server,Socket } from 'socket.io';
-
-type Data = {
-    status:Number
-    payload:Object
-}
+import { socketRoutes,testData } from '../utils'
 
 // find way to globalize all data being sent from client and backend and the types of the data being sent
-@WebSocketGateway(80, {namespace: 'chat', cors:{origin:{global}}})
+@WebSocketGateway(80, {namespace: 'chat',cookie:true,cors:{origin:{global}}})
 export class SocketIoGateway implements OnGatewayConnection{
     @WebSocketServer()
     server:Server;
-    
-    handleConnection(client: Socket): Data {
-        return { status: 200, payload: {message:"Connection is successful"}}
+    allRooms = new Set([]);
+    vacantRooms = [];
+    handleConnection(client: Socket): void {
+        console.log('connection is succesful')
     }
+    @SubscribeMessage(socketRoutes.joinRoom)
+    handleJoinRoom(@MessageBody() data:string, @ConnectedSocket() client : Socket, @Res({passthrough:true}) response : Response): string{
+        const currentRoom = [...client.rooms.keys()][0]
+        client.leave(currentRoom)
+        if(this.vacantRooms.length == 0){
+            let roomId = "-1"
+            do{
+                roomId = ""+Math.ceil(Math.random() * 10000)
+            }while(this.allRooms.has(roomId));
+            this.vacantRooms = [roomId,...(this.vacantRooms)]
+            this.allRooms.add(roomId)
+            client.join(roomId)
+            console.log(response)
+            return roomId
+        }else{
+            const roomId = this.vacantRooms[0]
+            this.vacantRooms = this.vacantRooms.slice(1,-1)
+            client.join(roomId)
+            return roomId
+        }
+    }
+
+    @SubscribeMessage(socketRoutes.sendTestData)
+    handleSendTestData(@MessageBody() data:testData, @ConnectedSocket() client : Socket): void{
+        const currentRoom = [...client.rooms.keys()][0]
+        console.log(currentRoom)
+        this.server.to(currentRoom).emit(socketRoutes.sendTestData,data)
+    }
+
 }
---
+++ src/utils/socket-routes.ts
@@ -0,0 +1,10 @@
+const socketRoutes = {
+    joinRoom:'joinRoom',
+    sendTestData:'sendTestData'
+}
+
+export type testData = {
+    message:string,
+}
+
+export { socketRoutes }
